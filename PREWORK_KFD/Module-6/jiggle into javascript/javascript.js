d54fd6be5adba6a1d5775aa0112b4fb37799b803
kirafaye99 <82430185+kirafaye99@users.noreply.github.com>
2021-06-05 18:39:22 -0700

    Add files via upload

---
+++ PREWORK_KFD/Module-6/jiggle into javascript/javascript.js
@@ -0,0 +1,13 @@
+function grow(id) {
+    id.style.width = "200px";
+    id.style.height = "200px";
+}
+function blue(id) {
+    id.style.backgroundColor = "blue";
+}
+function fade(id) {
+    id.style.opacity = "0.5";
+}
+function reset(id) {
+    id.style = "height:150px; width:150px; background-color:orange; margin:25px; opacity:1.0";
+}
